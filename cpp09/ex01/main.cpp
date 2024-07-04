050f31c895c16c300b3e9857ca2807355fbbabc4
Ramy <ramymohamedmousa@gmail.com>
2024-07-04 18:24:35 +0200

    RPN is ready

---
+++ cpp09/ex01/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "RPN.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./RPN \"EXPRESSION HERE\"" << std::endl;
+		return 1;
+	}
+	try
+	{
+		RPN rpn = RPN(argv[1]);
+		std::cout << rpn.compute(argv[1]) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	return 0;
+}
